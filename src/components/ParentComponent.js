da4132fcdfb42c23f31568a6f440363013db4f31
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-05-25 06:53:12 +0000

    Done.

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Form from './Form';
+import DispalyData from './DispalyData'
+
+export default class ParentComponent extends React.Component {
+  state = {
+    firstName: "",
+    lastName: "",
+  }
+  
+  // handleFirstNameChange = event => {
+  //   this.setState({
+  //     firstName: event.target.value
+  //   })
+  // }
+  
+  // handleLastNameChange = event => {
+  //   this.setState({
+  //     lastName: event.target.value
+  //   })
+  // }
+  
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  
+  render() {
+    return (
+    <Form formData={this.state} handleChange={this.handleChange}
+    />
+    <DispalyData formData={this.state}/>
+    )
+  }
+}
