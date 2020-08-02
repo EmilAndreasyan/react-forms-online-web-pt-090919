781931ee13277588d7eec551f3481f5a75c43060
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-08-02 20:30:14 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -55,8 +55,8 @@ class Form extends React.Component {
     return (
       <div>
       <form>
-      <input type="text" onChange={event => this.props.handleFirstNameChange} value={this.props.formData.firstName}/>
-       <input type="text" onChange={event => this.props.handleLastNameChange} value={this.props.formData.lastName}/>
+      <input type="text" onChange={event => this.props.handleChange} value={this.props.formData.firstName}/>
+       <input type="text" onChange={event => this.props.handleChange} value={this.props.formData.lastName}/>
       <form/>
       </div>
       )
